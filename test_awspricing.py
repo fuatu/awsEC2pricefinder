f2f94d950f3ee1b037fda330cc0b6831b478e138
Fuat Ulugay <fuat.ulugay@gmail.com>
2020-05-25 23:58:31 +0300

    add additional test

--- test_awspricing.py
+++ test_awspricing.py
@@ -1,4 +1,14 @@
 from includes import print_help
+from includes import read_yaml
 
 def test_print_help():
     assert print_help()
+
+def test_read_yaml():
+    # positive test
+    filename = 'credentials.yaml'
+    assert not read_yaml(filename) is None
+    # negative test
+    filename = 'credentialsx.yaml'
+    assert read_yaml(filename) is None
+
